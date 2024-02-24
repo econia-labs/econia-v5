7ffd16bc5d49a675cd501400c6b5cb4a94e07b48
alnoki <43892045+alnoki@users.noreply.github.com>
2024-02-23 18:24:22 -0800

    Run shfmt

--- src/sh/move-source-coverage.sh
+++ src/sh/move-source-coverage.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 (
-    clear
-    local module=$1
+	clear
+	local module=$1
 	local move_dir=src/move/econia
-    aptos move coverage source --dev --module $module --package-dir $move_dir
+	aptos move coverage source --dev --module $module --package-dir $move_dir
 )
