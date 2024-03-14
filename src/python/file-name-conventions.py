26e145ca9cd71843feab52509c647266d98b2d80
xbtmatt <xbtchunky@gmail.com>
2024-03-13 19:30:08 -0700

    Change script to run only on the files passed in from `--all-files` instead of running on each file in `git ls-files` to avoid redundant processing

--- src/python/file-name-conventions.py
+++ src/python/file-name-conventions.py
@@ -1,6 +1,5 @@
 #!/usr/bin/env python3
 import yaml
-import subprocess
 import re
 import sys
 from pathlib import Path
@@ -38,22 +37,17 @@ def main():
         print('Unrecognized naming conventions:', ', '.join(unrecognized_conventions))
         sys.exit(1)
 
-    # Retrieve all tracked files
-    cmd = ['git', 'ls-files']
-    files = subprocess.check_output(cmd).decode().splitlines()
+    files = sys.argv[1:]
 
     errors = False
-    print('Duplicate files in `git ls-files` output:', len(files) - len(set(files)))
-    i = 0
     for file_path in files:
         extension = file_path.split('.')[-1] if '.' in file_path else ''
         case = filetypes.get(extension, default_case)
         regex = NAMING_CONVENTIONS.get(case, default_case)
 
         if not check_file_naming(Path(file_path), regex):
-            print(f'i: {i} -> Error: {file_path} does not follow {case} naming convention')
+            print(f'Error: {file_path} does not follow {case} naming convention')
             errors = True
-        i += 1
 
     if errors:
         sys.exit(1)
