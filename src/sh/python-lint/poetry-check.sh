35ff10459cbd0a8b11b795a57189e37d26e0c2e5
xbtmatt <xbtchunky@gmail.com>
2024-03-29 15:41:42 -0700

    Add conditional poetry install to `poetry-check.sh` by checking GITHUB_ACTIONS env variable

--- src/sh/python-lint/poetry-check.sh
+++ src/sh/python-lint/poetry-check.sh
@@ -1,17 +1,27 @@
 #!/bin/sh
 
-POETRY_SUBDIRECTORY=./src/python/hooks
+if [ "$GITHUB_ACTIONS" = "true" ]; then
+	echo 'GitHub Actions environment detected, skipping `poetry check`...'
+	exit 0
+fi
 
 poetry --version >/dev/null 2>&1 || {
 	echo "Poetry is not installed. Please install poetry to continue."
 	exit 1
 }
 
+POETRY_SUBDIRECTORY=./src/python/hooks
 cd $POETRY_SUBDIRECTORY || exit 1
 
-poetry check --quiet || {
-	echo "Poetry check failed."
-	exit 1
+# Run `poetry check` first, `poetry lock --check` in case their version of poetry
+# doesn't support `poetry check`.
+poetry check --lock --quiet || poetry lock --check --no-update --quiet || {
+	echo "Poetry lock file is out of date. Running poetry install."
+	poetry install --no-root || {
+		echo "Poetry install failed."
+		exit 1
+	}
+	exit 0
 }
 
 exit 0
