0b1a3e82d1470673bad2b5487229105364d7e774
xbtmatt <xbtchunky@gmail.com>
2024-03-29 17:48:03 -0700

    Ensure CI runs `poetry check` in poetry-check.sh, rather than skipping entirely

--- src/sh/python-lint/poetry-check.sh
+++ src/sh/python-lint/poetry-check.sh
@@ -10,8 +10,19 @@ function cleanup() {
 # Ensure cleanup function is called on exit.
 trap cleanup EXIT
 
+ROOT_DIR=$(git rev-parse --show-toplevel)
+PYTHON_DIR=$ROOT_DIR/src/python
+POETRY_SUBDIRECTORY=$PYTHON_DIR/hooks
+cd $POETRY_SUBDIRECTORY || exit 1
+
+# In CI, we only ensure that the lock file is in sync with `pyproject.toml`.
+# Poetry should already be installed at this point.
 if [ "$GITHUB_ACTIONS" == "true" ]; then
-	echo 'GitHub Actions environment detected, skipping `poetry check`...'
+	poetry check --lock || {
+		echo 'Poetry lock file is out of date. Running `poetry lock --no-update`...'
+		exit 1
+	}
+	echo 'Poetry dependencies in sync, skipping the rest of `poetry check`...'
 	exit 0
 fi
 
@@ -19,11 +30,6 @@ poetry --version >/dev/null 2>&1 || {
 	echo "Poetry is not installed. Please install poetry to continue."
 	exit 1
 }
-
-ROOT_DIR=$(git rev-parse --show-toplevel)
-PYTHON_DIR=$ROOT_DIR/src/python
-POETRY_SUBDIRECTORY=$PYTHON_DIR/hooks
-cd $POETRY_SUBDIRECTORY || exit 1
 
 # Attempt to install the poetry dependencies.
 poetry install --no-root || {
@@ -34,14 +40,12 @@ poetry install --no-root || {
 # Check if `poetry.lock` is consistent with `pyproject.toml`.
 # We run both commands in case their version of poetry doesn't
 # support `poetry check --lock`.
-poetry check --lock || poetry lock --check --no-update || {
+poetry check --lock || poetry lock --check || {
 	echo 'Poetry lock file is out of date. Running `poetry lock --no-update`...'
 	poetry lock --no-update || {
 		echo "Poetry lock failed."
 		exit 1
 	}
-	echo "Exiting successfully..?"
-	exit 0
 }
 
 exit 0
