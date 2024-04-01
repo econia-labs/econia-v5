055aff4ab631da9ebb5afad92fb77ba95091f419
xbtmatt <xbtchunky@gmail.com>
2024-03-31 17:40:49 -0700

    Ensure format.sh works correctly when sourced or run in a shell

--- src/sh/python-lint/format.sh
+++ src/sh/python-lint/format.sh
@@ -1,11 +1,7 @@
 #!/bin/sh
 # cspell:words toplevel
 
-# Exit if any command fails.
-set -e
-
 ROOT_DIR=$(git rev-parse --show-toplevel)
 DIR_ABS_PATH="$ROOT_DIR/src/sh/python-lint"
 
-"$DIR_ABS_PATH/poetry-check.sh"
-"$DIR_ABS_PATH/run-in-poetry-subdir.sh" "poetry run python -m format"
+"$DIR_ABS_PATH/poetry-check.sh" && "$DIR_ABS_PATH/run-in-poetry-subdir.sh" "poetry run python -m format"
