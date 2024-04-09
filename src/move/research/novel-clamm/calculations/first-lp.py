0cab0ae99bfff331a53347a94cfa4e99ef1e0714
alnoki <43892045+alnoki@users.noreply.github.com>
2024-04-09 18:39:54 +0800

    Add price for first LP

--- src/move/research/novel-clamm/calculations/first-lp.py
+++ src/move/research/novel-clamm/calculations/first-lp.py
@@ -16,15 +16,19 @@ def get_q_r(L, b_r):
     return ((L ** 2) * (sqrt_p_h - sqrt_p_l) - L * b_r * sqrt_p_h * sqrt_p_l) / \
     (b_r * sqrt_p_h + L)
 
+def get_sqrt_p(L, b_r, q_r):
+    return math.sqrt((sqrt_p_h * (q_r + L * sqrt_p_l)) / (b_r * sqrt_p_h + L))
+
 sqrt_p_l = 10.0
 sqrt_p_h = 20.0
 b_r = 200.0
 q_r = 5000.0
+L = get_L(b_r, q_r)
 
 print(f"sqrt_p_h: {sqrt_p_h}, sqrt_p_l: {sqrt_p_l}")
 print(f"Initial b_r: {b_r}, q_r: {q_r}")
 
-L = get_L(b_r, q_r)
 print(f"Resultant liquidity L: {L}")
 print(f"b_r from L and q_r: {get_b_r(L, q_r)}")
 print(f"q_r from L and b_r: {get_q_r(L, b_r)}")
+print(f"sqrt_p from L, b_r, and q_r: {get_sqrt_p(L, b_r, q_r)}")
